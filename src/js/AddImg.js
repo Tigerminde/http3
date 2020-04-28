5822e43255cda58d0b573ff1494c699309bd1ebf
AppVeyor <ci@appveyor.com>
2020-04-28 12:39:09 +0300

    first commit

---
+++ src/js/AddImg.js
@@ -0,0 +1,28 @@
+export default class AddImg {
+  constructor(blockImg, elementError) {
+    this.blockImg = blockImg;
+    this.elementError = elementError;
+  }
+
+  createImg(name, url) {
+    const addImgElement = document.createElement('img');
+    addImgElement.src = url;
+
+    addImgElement.addEventListener('load', () => {
+      this.elementError.classList.add('hidden');
+      addImgElement.className = 'item-img';
+      addImgElement.alt = name;
+
+      const addDivImg = document.createElement('div');
+
+      addDivImg.className = 'item-img-div';
+      addDivImg.innerHTML = '<div class="close">✖</div>';
+      addDivImg.appendChild(addImgElement);
+      this.blockImg.appendChild(addDivImg);
+    });
+
+    addImgElement.addEventListener('error', () => {
+      this.elementError.classList.remove('hidden');
+    });
+  }
+}
